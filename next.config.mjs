a0446dcf375157a8c3c5fd89201ba10adc9d255d
Ingrid Lizana <ieg.lizana@gmail.com>
2025-11-21 18:53:56 -0300

    fix: add unoptimized images config for debugging

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        unoptimized: true,
+    },
     webpack: (config, { isServer, nextRuntime }) => {
         if (isServer && nextRuntime === "edge") {
             config.resolve.alias = {
